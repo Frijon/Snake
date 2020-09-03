613c9ee07d98140b81d01a85c06262408101f4f3
Orios <barinthe1st@gmail.com>
2020-09-03 20:48:33 +0200

    changed logic -> updating positions from front to back, last is now head

--- snake.js
+++ snake.js
@@ -8,41 +8,38 @@ class Snake {
 			x: (400),
 			y: (400),
 		}
-
-		this.dir = "left";
 	}
 
 	update() {
-		if ((frame % 10) == 0) {
-
-			for (let i = 0; i < snake.length; ++i) {
-				if (snake.length != 1){
-					snake[snake.length -1 - i].pos.x = snake[snake.length - 2].pos.x;
-					snake[i].pos.y = snake[i + 1].pos.y;
-
+		if ((frame % 20) == 0) {
+			if (snake.length != 1) {
+				for (let i = 0; i < snake.length -1; ++i) {
+						//giving position from front to back
+						snake[i].pos.x = snake[i + 1].pos.x;
+						snake[i].pos.y = snake[i + 1].pos.y;
 				}
 			}
 
-			switch (this.dir) {
+
+			switch (dir) {
 				case "left":
-					this.pos.x += -10;
+					snake[snake.length -1].pos.x += -10;
 					break;
 				case "right":
-					this.pos.x += 10;
+					snake[snake.length -1].pos.x += 10;
 					break;
 				case "up":
-					this.pos.y += -10;
+					snake[snake.length -1].pos.y += -10;
 					break;
 				case "down":
-					this.pos.y += 10;
+					snake[snake.length -1].pos.y += 10;
 					break;
+				}
+				//control loop
+			for (let i = 0; i < snake.length; ++i){
+				console.log("snake[" + [i] +"].pos.x, y = " + snake[i].pos.x + ", " + snake[i].pos.y);
 			}
 		}
-	}
-
-	addbit(){
-		var i = body.length;
-		body[i] = new Snake(800, 500);
 	}
 
 	draw() {
