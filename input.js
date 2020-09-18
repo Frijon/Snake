25d28a293c7ccf1ac84550668c4a6158dc579f62
Orios <barinthe1st@gmail.com>
2020-09-18 18:14:11 +0200

    removed 360 turns when body is longer than 1

--- input.js
+++ input.js
@@ -1,21 +1,58 @@
 function keyPressed() {
 
-	switch (keyCode) {
-		case UP_ARROW:
-			dir = "up";
-			break;
-		case DOWN_ARROW:
-			dir = "down";
-			break;
-		case LEFT_ARROW:
-			dir = "left";
-			break;
-		case RIGHT_ARROW:
-			dir = "right";
-			break;
-		case 71:
-			Snake.addbit();
-			break;
+	if (snake.length != 1){
+		switch (keyCode) {
+			case UP_ARROW:
+				if (dir == "down"){
+					break;
+				} else {
+					dir = "up";
+				}
+				break;
+			case DOWN_ARROW:
+				if (dir == "up"){
+					break;
+				} else {
+					dir = "down";
+				}
+				break;
+			case LEFT_ARROW:
+				if (dir == "right") {
+					break;
+				} else {
+					dir = "left";
+				}
+				break;
+			case RIGHT_ARROW:
+				if (dir == "left") {
+					break;
+				} else {
+					dir = "right";
+				}
+				break;
+			case 71:
+				Snake.addbit();
+				break;
+		}
+	} else {
+		switch (keyCode) {
+			case UP_ARROW:
+				dir = "up";
+				break;
+			case DOWN_ARROW:
+				dir = "down";
+				break;
+			case LEFT_ARROW:
+				dir = "left";
+				break;
+			case RIGHT_ARROW:
+				dir = "right";
+				break;
+			case 71:
+				Snake.addbit();
+				break;
+		}
 	}
+
 	return false;
 }
